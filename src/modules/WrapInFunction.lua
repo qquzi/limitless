532863edd5871d25fcaca66e9b8e4053eb7d2cf2
qquzi <anonymoususer29211@gmail.com>
2026-06-03 15:55:23 -0700

    Update WrapInFunction.lua

--- src/modules/WrapInFunction.lua
+++ src/modules/WrapInFunction.lua
@@ -1,5 +1,26 @@
 local Wrapper = {}
-function Wrapper.process(code)
-return [[return (function(...) ]]..code..[[ end)(...)]]
+
+local function randomName(length)
+	local chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	local out = {}
+
+	for i = 1, length do
+		local r = math.random(1, #chars)
+		out[i] = chars:sub(r, r)
+	end
+
+	return table.concat(out)
 end
+
+function Wrapper.process(code)
+	local wrapperName = randomName(math.random(12, 24))
+
+	return string.format([[
+local function %s(...)
+%s
+end
+return %s(...)
+]], wrapperName, code, wrapperName)
+end
+
 return Wrapper
