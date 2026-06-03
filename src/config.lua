49e91e5dfacb70e15db8ba59ee36cbc3fd367829
qquzi <anonymoususer29211@gmail.com>
2026-06-03 11:00:17 -0700

    Update config.lua

--- src/config.lua
+++ src/config.lua
@@ -3,51 +3,70 @@
 local config = {}
 
 config.settings = {
-    output_suffix = "_obfuscated.lua",
-    watermark_enabled = true,
+    output_suffix = "_limitless.lua",
+
+    watermark_enabled = false,
     final_print = true,
+
+    Limitless = {
+        build = "private",
+        version = "1.0.0",
+    },
+
     VirtualMachine = {
         enabled = true,
     },
+
     antitamper = {
         enabled = true,
     },
+
     control_flow = {
         enabled = true,
-        max_fake_blocks = 6,
+        max_fake_blocks = 12,
     },
+
     StringToExpressions = {
-        enabled = false,
-        min_number_length = 100,
-        max_number_length = 999,
+        enabled = true,
+        min_number_length = 250,
+        max_number_length = 1200,
     },
+
     string_encoding = {
-        enabled = false,
+        enabled = true,
     },
+
     WrapInFunction = {
         enabled = true,
     },
+
     variable_renaming = {
         enabled = true,
-        min_name_length = 8,
-        max_name_length = 12,
+        min_name_length = 16,
+        max_name_length = 32,
     },
+
     garbage_code = {
         enabled = true,
-        garbage_blocks = 20,
+        garbage_blocks = 35,
     },
+
     opaque_predicates = {
         enabled = true,
     },
+
     function_inlining = {
-        enabled = false,
+        enabled = true,
     },
+
     dynamic_code = {
-        enabled = false,
+        enabled = true,
     },
+
     bytecode_encoding = {
-        enabled = false,
+        enabled = true,
     },
+
     compressor = {
         enabled = true,
     }
@@ -55,37 +74,49 @@ config.settings = {
 
 function config.get(key)
     local keys = {}
-    for k in key:gmatch("[^.]+") do table.insert(keys, k) end
+
+    for k in key:gmatch("[^.]+") do
+        table.insert(keys, k)
+    end
 
     local value = config
+
     for _, k in ipairs(keys) do
         value = value[k]
+
         if value == nil then
             return nil
         end
     end
+
     return value
 end
 
 function config.set(key, new_value)
     local keys = {}
-    for k in key:gmatch("[^.]+") do table.insert(keys, k) end
+
+    for k in key:gmatch("[^.]+") do
+        table.insert(keys, k)
+    end
 
     local value = config
+
     for i = 1, #keys - 1 do
         value = value[keys[i]]
+
         if value == nil then
             return false
         end
     end
 
     local last_key = keys[#keys]
+
     if value[last_key] ~= nil then
         value[last_key] = new_value
         return true
-    else
-        return false
     end
+
+    return false
 end
 
 return config
