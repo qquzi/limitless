988f1bad5c31ff9ff80a7b357d6cf9f73cd0ab61
qquzi <anonymoususer29211@gmail.com>
2026-06-03 16:33:26 -0700

    Update string_encoder.lua

--- src/modules/string_encoder.lua
+++ src/modules/string_encoder.lua
@@ -1,113 +1,78 @@
 local StringEncoder = {}
 
-local function generateRandomName(len)
-    len = len or math.random(8, 12)
-    local charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    local name = ""
-    for _ = 1, len do
-        local index = math.random(1, #charset)
-        name = name .. charset:sub(index, index)
+-- idk decoder
+
+local function randName()
+    local chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    local s = {}
+    for i = 1, math.random(8, 12) do
+        local c = chars:sub(math.random(1, #chars), math.random(1, #chars))
+        s[#s+1] = c
     end
-    return name
+    return table.concat(s)
 end
 
-local function isValidChar(byte)
-    return (byte >= 48 and byte <= 57) or (byte >= 65 and byte <= 90) or (byte >= 97 and byte <= 122)
-end
+local function caesar(str, offset)
+    local out = {}
 
-local function caesarCipher(data, offset)
-    local result = {}
-    local i = 1
-    while i <= #data do
-        local byte = data:byte(i)
-        if byte == 92 and i < #data then
-            local next_char = data:sub(i + 1, i + 1)
-            if next_char == "2" and data:sub(i+2,i+2) == "7" then
-                table.insert(result, string.char(byte))
-                table.insert(result, next_char)
-                table.insert(result, data:sub(i+2,i+2))
-                i = i + 2
-            else
-                table.insert(result, string.char(byte))
-                table.insert(result, next_char)
-                i = i + 1
-            end
-        elseif isValidChar(byte) then
-            local new_byte
-            if byte >= 48 and byte <= 57 then
-                new_byte = ((byte - 48 + offset) % 10) + 48
-            elseif byte >= 65 and byte <= 90 then
-                new_byte = ((byte - 65 + offset) % 26) + 65
-            elseif byte >= 97 and byte <= 122 then
-                new_byte = ((byte - 97 + offset) % 26) + 97
-            end
-            table.insert(result, string.char(new_byte))
-        else
-            table.insert(result, string.char(byte))
+    for i = 1, #str do
+        local b = str:byte(i)
+
+        if b >= 48 and b <= 57 then
+            b = ((b - 48 + offset) % 10) + 48
+        elseif b >= 65 and b <= 90 then
+            b = ((b - 65 + offset) % 26) + 65
+        elseif b >= 97 and b <= 122 then
+            b = ((b - 97 + offset) % 26) + 97
         end
 
-        i = i + 1
+        out[#out+1] = string.char(b)
     end
-    return table.concat(result)
+
+    return table.concat(out)
 end
 
 function StringEncoder.process(code)
-    local random_decrypt_name = generateRandomName()
-    local random_isvalidchar_name = generateRandomName()
-    local random_result_name = generateRandomName()
-    local random_code_name = generateRandomName()
-    local random_offset_name = generateRandomName()
-    local random_byte_name = generateRandomName()
-    local random_new_byte_name = generateRandomName()
+    local decoderName = randName()
 
-    local decode_function = [[
-local function ]] .. random_isvalidchar_name .. [[(]] .. random_byte_name .. [[)
-    return (]] .. random_byte_name .. [[ >= 48 and ]] .. random_byte_name .. [[ <= 57) or (]] .. random_byte_name .. [[ >= 65 and ]] .. random_byte_name .. [[ <= 90) or (]] .. random_byte_name .. [[ >= 97 and ]] .. random_byte_name .. [[ <= 122)
-end
-	
-local function ]] .. random_decrypt_name .. [[(]] .. random_code_name .. [[, ]] .. random_offset_name .. [[)
-    local ]] .. random_result_name .. [[ = {}
-    for i = 1, #]] .. random_code_name .. [[ do
-        local ]] .. random_byte_name .. [[ = ]] .. random_code_name .. [[:byte(i)
-        if ]] .. random_isvalidchar_name .. [[(]] .. random_byte_name .. [[) then
-            local ]] .. random_new_byte_name .. [[
-            if ]] .. random_byte_name .. [[ >= 48 and ]] .. random_byte_name .. [[ <= 57 then
-                ]] .. random_new_byte_name .. [[ = ((]] .. random_byte_name .. [[ - 48 - ]] .. random_offset_name .. [[ + 10) % 10) + 48
-            elseif ]] .. random_byte_name .. [[ >= 65 and ]] .. random_byte_name .. [[ <= 90 then
-                ]] .. random_new_byte_name .. [[ = ((]] .. random_byte_name .. [[ - 65 - ]] .. random_offset_name .. [[ + 26) % 26) + 65
-            elseif ]] .. random_byte_name .. [[ >= 97 and ]] .. random_byte_name .. [[ <= 122 then
-                ]] .. random_new_byte_name .. [[ = ((]] .. random_byte_name .. [[ - 97 - ]] .. random_offset_name .. [[ + 26) % 26) + 97
-            end
-            table.insert(]] .. random_result_name .. [[, string.char(]] .. random_new_byte_name .. [[))
-        else
-            table.insert(]] .. random_result_name .. [[, string.char(]] .. random_byte_name .. [[))
+    local decodeFn = string.format([[
+local function %s(str, offset)
+    local out = {}
+    for i = 1, #str do
+        local b = str:byte(i)
+
+        if b >= 48 and b <= 57 then
+            b = ((b - 48 - offset + 10) %% 10) + 48
+        elseif b >= 65 and b <= 90 then
+            b = ((b - 65 - offset + 26) %% 26) + 65
+        elseif b >= 97 and b <= 122 then
+            b = ((b - 97 - offset + 26) %% 26) + 97
         end
+
+        out[#out+1] = string.char(b)
     end
-    return table.concat(]] .. random_result_name .. [[)
+    return table.concat(out)
 end
+]], decoderName)
 
-local function ]] .. random_isvalidchar_name .. [[(]] .. random_byte_name .. [[)
-    return (]] .. random_byte_name .. [[ >= 48 and ]] .. random_byte_name .. [[ <= 57) or (]] .. random_byte_name .. [[ >= 65 and ]] .. random_byte_name .. [[ <= 90) or (]] .. random_byte_name .. [[ >= 97 and ]] .. random_byte_name .. [[ <= 122)
-end
-]]
-    code = code:gsub('\\"', '!@!'):gsub("\\'", "@!@")
+    -- string capture
+    code = code:gsub("\\\\", "\0ESCAPE\0")
 
-    code = code:gsub("(['\"])(.-)%1", function(quote,str)
-        if type(str) == "string" then
-            str = str:gsub('!@!', '\\"'):gsub('@!@', "\\'")
-
-            local offset = math.random(1, 9)
-            if str:match("%a") then
-                offset = math.random(1, 25)
-            end
-            local encoded_str = caesarCipher(str, offset)
-            return string.format("%s(" .. quote .. "%s" .. quote .. ", %d)", random_decrypt_name, encoded_str, offset)
-        else
-            return str
-        end
+    code = code:gsub([["(.-)"]], function(str)
+        local offset = math.random(1, 25)
+        local encoded = caesar(str, offset)
+        return string.format('%s("%s", %d)', decoderName, encoded, offset)
     end)
 
-    return decode_function .. "\n" .. code
+    code = code:gsub([['(.-)']], function(str)
+        local offset = math.random(1, 25)
+        local encoded = caesar(str, offset)
+        return string.format("%s('%s', %d)", decoderName, encoded, offset)
+    end)
+
+    code = code:gsub("\0ESCAPE\0", "\\\\")
+
+    return decodeFn .. "\n" .. code
 end
 
 return StringEncoder
